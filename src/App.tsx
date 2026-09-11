a0893b3086d2b1122f5de8419cc9efdb332b749d
krishna3052002 <shukrishna23@gmail.com>
2026-09-11 10:21:37 +0600

    project setup completed

--- src/App.tsx
+++ src/App.tsx
@@ -16,3 +16,11 @@ function App() {
 }
 
 export default App
+
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/krishnasaha-30/devstack-app.git
+// git push -u origin main
